f186e870fb93d36c3a937192e0e22c5046cc8b40
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-08-02 20:31:27 +0300

    add class that include calculator logic

---
+++ src/main/java/org/example/mathFunctions/CalculatorLogic.java
@@ -0,0 +1,19 @@
+package org.example.mathFunctions;
+
+public class CalculatorLogic {
+    public double sumNumbers(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subNumbers(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double divNumbers(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public double mulNumbers(double num1, double num2) {
+        return num1 * num2;
+    }
+}
