6e5512147f2c808c9f49b8547ff011ef6817fdfa
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-08-02 20:32:08 +0300

    add test all calculator functions

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,54 @@
+import org.example.mathFunctions.CalculatorLogic;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalcTest {
+    @Test
+    public void testSum() {
+        CalculatorLogic cl = new CalculatorLogic();
+        double value1 = 5;
+        double value2 = 4;
+
+        final double result = cl.sumNumbers(value1, value2);
+
+        final double expected = 5 + 4;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSub() {
+        CalculatorLogic cl = new CalculatorLogic();
+        double value1 = 5;
+        double value2 = 4;
+
+        final double result = cl.subNumbers(value1, value2);
+
+        final double expected = 5 - 4;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDiv() {
+        CalculatorLogic cl = new CalculatorLogic();
+        double value1 = 5;
+        double value2 = 4;
+
+        final double result = cl.divNumbers(value1, value2);
+
+        final double expected = 5 / 4.0;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMul() {
+        CalculatorLogic cl = new CalculatorLogic();
+        double value1 = 5;
+        double value2 = 4;
+
+        final double result = cl.mulNumbers(value1, value2);
+
+        final double expected = 5 * 4;
+        assertEquals(expected, result);
+    }
+}
