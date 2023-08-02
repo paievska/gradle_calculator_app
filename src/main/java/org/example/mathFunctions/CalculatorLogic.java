0f5a3cf6dd57dfbc3c16fce1b05e6ef3d175a5fb
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-08-02 20:56:38 +0300

    add checking for division by zero

--- src/main/java/org/example/mathFunctions/CalculatorLogic.java
+++ src/main/java/org/example/mathFunctions/CalculatorLogic.java
@@ -1,6 +1,8 @@
 package org.example.mathFunctions;
 
+
 public class CalculatorLogic {
+
     public double sumNumbers(double num1, double num2) {
         return num1 + num2;
     }
@@ -10,6 +12,9 @@ public class CalculatorLogic {
     }
 
     public double divNumbers(double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("You divide by zero");
+        }
         return num1 / num2;
     }
 
