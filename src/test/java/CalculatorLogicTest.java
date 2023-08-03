0dfeef8c02d3a9f5766d025c76def5c7fc988610
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-08-04 01:03:14 +0300

    rename class

--- src/test/java/CalculatorLogicTest.java
+++ src/test/java/CalculatorLogicTest.java
@@ -1,4 +1,3 @@
-import org.example.mathFunctions.CalculatorLogic;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -6,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CalcTest {
+public class CalculatorLogicTest {
     private static CalculatorLogic cl;
     @BeforeEach
     public void setUp(){
@@ -18,33 +17,36 @@ public class CalcTest {
     }
     @Test
     public void testSum() {
+        //Given
         double value1 = 5;
         double value2 = 4;
-
+        //When
         final double result = cl.sumNumbers(value1, value2);
-
+        //Then
         final double expected = 5 + 4;
         assertEquals(expected, result);
     }
 
     @Test
     public void testSub() {
+        //Given
         double value1 = 5;
         double value2 = 4;
-
+        //When
         final double result = cl.subNumbers(value1, value2);
-
+        //Then
         final double expected = 5 - 4;
         assertEquals(expected, result);
     }
 
     @Test
     public void testDiv() {
+        //Given
         double value1 = 5;
         double value2 = 4;
-
+        //When
         final double result = cl.divNumbers(value1, value2);
-
+        //Then
         final double expected = 5 / 4.0;
         assertEquals(expected, result);
         assertThrows(IllegalArgumentException.class, () -> cl.divNumbers(value1, 0.0));
@@ -52,11 +54,12 @@ public class CalcTest {
 
     @Test
     public void testMul() {
+        //Given
         double value1 = 5;
         double value2 = 4;
-
+        //When
         final double result = cl.mulNumbers(value1, value2);
-
+        //Then
         final double expected = 5 * 4;
         assertEquals(expected, result);
     }
